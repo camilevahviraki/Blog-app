2f5e3162dc589f8e3d777835560c35fefb2ac691
camilevahviraki <camilevahviraki@gmail.com>
2022-08-16 15:37:06 +0200

    Add post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,9 @@
 class PostsController < ApplicationController
-  def update_posts_count(user)
-    post = Post.where(author_id: user)
-    users = User.find(user)
-    users.update(PostsCounter: post.length)
+  def index
+
   end
 
-  def recents_comments(post)
-    Comment.where(posts_id: post).last(5)
-  end
-end
+  def show
+     
+  end 
+end    
