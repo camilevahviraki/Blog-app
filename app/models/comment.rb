511fb4ad38a91b66f15f1a4e04f8285eb9778702
camilevahviraki <camilevahviraki@gmail.com>
2022-08-16 15:33:30 +0200

    Update models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,10 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  def update_comment_count(post)
+    comment = Comment.where(posts_id: post)
+    posts = Post.find(post)
+    posts.update(CommentsCounter: comment.length)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -6,4 +6,14 @@ class Post < ApplicationRecord
                               too_long: '%<count>s characters is the maximum allowed' }
   validates :LikesCounter, numericality: true
   validates :LikesCounter, comparison: { greater_than_or_equal_to: 0 }
+
+  def update_posts_count(user)
+    post = Post.where(author_id: user)
+    users = User.find(user)
+    users.update(PostsCounter: post.length)
+  end
+
+  def recents_comments(post)
+    Comment.where(posts_id: post).last(5)
+  end
 end
