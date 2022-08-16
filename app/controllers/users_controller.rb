131726239e0bd8d62c19a117a101333f622e5ac9
camilevahviraki <camilevahviraki@gmail.com>
2022-08-16 15:32:53 +0200

    Add users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
-  def last_3_post(author)
-    Post.where(author_id: author).last(3)
+  def index
+    # @users = User.all
   end
+
+  def show
+
+  end  
 end
