56efb53149b719be9b031c098d4721cc3e4a3489
camilevahviraki <camilevahviraki@gmail.com>
2022-08-19 20:18:47 +0200

    Update controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,14 @@
 class UsersController < ApplicationController
   def index
-    # @users = User.all
+    @users = User.all
   end
 
-  def show; end
+  def show
+    likes = Like.new
+    likes.update_like_counter
+    @user = User.find(params[:id])
+    post = User.new
+    @last_posts = post.last_3_post(params[:id])
+    @all_posts = Post.where(author_id: params[:id])
+  end
 end
