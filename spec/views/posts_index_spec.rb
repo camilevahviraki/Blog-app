c28af4ff619a32f885c95b5c40fd7c750a5c96f7
camilevahviraki <camilevahviraki@gmail.com>
2022-08-24 14:17:23 +0200

    Add tests for posts/index

---
+++ spec/views/posts_index_spec.rb
@@ -0,0 +1,56 @@
+require 'rails_helper'
+
+RSpec.describe "Users_Index", type: :feature do
+   
+  subject { User.new(Name: 'camilux', Photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQx5pB6py4twIFGBfFT4IS8E13iYj_pzGyKw&usqp=CAU', Bio: 'kefjwckllkl', PostsCounter: 0)}
+  
+    before :each do
+     subject.save
+     Post.create(Title: 'Test title1', Text: 'Test Text1', CommentsCounter: 1, LikesCounter: 0, author_id: User.find(subject.id).id)
+     Post.create(Title: 'Test title2', Text: 'Test Text2', CommentsCounter: 0, LikesCounter: 0, author_id: User.find(subject.id).id)
+     Post.create(Title: 'Test title3', Text: 'Test Text3', CommentsCounter: 0, LikesCounter: 0, author_id: User.find(subject.id).id)
+     Comment.create(Text: 'Comment number1 test', posts_id: Post.find_by(Title: 'Test title1').id, author_id: User.find(subject.id).id)
+    end
+  
+    it "User username" do
+      visit "/users/#{User.find(subject.id).id}/posts"
+      expect(page).to have_content 'camilux'
+    end
+
+    it "Check diplayed image link" do
+        visit "/users/#{User.find(subject.id).id}/posts"  
+        expect(page).to have_css("img[src*='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQx5pB6py4twIFGBfFT4IS8E13iYj_pzGyKw&usqp=CAU']")
+    end
+
+    it "See number of posts for user" do
+        visit "/users/#{User.find(subject.id).id}/posts"
+        expect(page).to have_content 'Number of posts: 0'
+    end
+
+    it "I can see a posts title" do
+        visit "/users/#{User.find(subject.id).id}/posts"
+        expect(page).to have_content 'Test title1'
+    end
+
+    it "I can see some of the posts body" do
+        visit "/users/#{User.find(subject.id).id}/posts"
+        expect(page).to have_content 'Test Text1...'
+    end
+
+    it "I can see the first comments on a post" do
+        visit "/users/#{User.find(subject.id).id}/posts"
+        expect(page).to have_content 'Comment number1 test'
+    end
+    
+    it "I can see how many comments a post has" do
+        visit "/users/#{User.find(subject.id).id}/posts"
+        expect(page).to have_content 'Comments:1 Likes: 0'
+    end  
+
+    it "When I click on a post, it redirects me to that post's show page." do
+        visit "/users/#{User.find(subject.id).id}/posts"
+        click_on('Test title1')
+        expect(page).to have_current_path("/users/#{User.find(subject.id).id}/posts/#{Post.find_by(Title: 'Test title1').id}")
+    end
+
+  end
