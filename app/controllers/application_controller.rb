062df0bf9b669d77e4038639e03b35607918853f
camilevahviraki <camilevahviraki@gmail.com>
2022-08-25 15:03:40 +0200

    Update in App folder and add styles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user
+    respond_to :html, :json
+    protect_from_forgery with: :exception
 
-  def current_user
-    @first_user = User.first
-  end
+    before_action :update_allowed_parameters, if: :devise_controller?
+  
+    protected
+  
+    def update_allowed_parameters
+      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:Name, :Photo, :email, :password, :Bio, :password_confirmation)}
+      devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:Name, :Bio, :Photo, :email, :password, :current_password)}
+    end
 end
