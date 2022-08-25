6dd3abf33e14fb258eeadc61972c46c6fda830a2
camilevahviraki <camilevahviraki@gmail.com>
2022-08-25 14:48:39 +0200

    Update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :likes
   has_many :posts
   has_many :comments
-  validates :Name, presence: true
-  validates :PostsCounter, numericality: true
-  validates :PostsCounter, comparison: { greater_than_or_equal_to: 0 }
+  # validates :Name, presence: true, allow_blank: true
+  # validates :PostsCounter, numericality: true
+  # validates :PostsCounter, comparison: { greater_than_or_equal_to: 0, allow_nil: true}
 
   def last_3_post(author)
     Post.where(author_id: author).last(3)
