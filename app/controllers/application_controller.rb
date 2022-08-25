7f4f974c7c61400f1871a9f32e0edea68098b4c1
Ogaga <iyaraogaga69@gmail.com>
2022-08-25 21:11:51 +0100

    fix linters errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,17 @@
 class ApplicationController < ActionController::Base
-    respond_to :html, :json
-    protect_from_forgery with: :exception
+  respond_to :html, :json
+  protect_from_forgery with: :exception
 
-    before_action :update_allowed_parameters, if: :devise_controller?
-  
-    protected
-  
-    def update_allowed_parameters
-      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:Name, :Photo, :email, :password, :Bio, :password_confirmation)}
-      devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:Name, :Bio, :Photo, :email, :password, :current_password)}
+  before_action :update_allowed_parameters, if: :devise_controller?
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:Name, :Photo, :email, :password, :Bio, :password_confirmation)
     end
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:Name, :Bio, :Photo, :email, :password, :current_password)
+    end
+  end
 end
