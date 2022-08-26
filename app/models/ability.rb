f6a15df396f2bb3f6a08d729d9172cc6018d9849
camilevahviraki <camilevahviraki@gmail.com>
2022-08-27 01:19:02 +0200

    Edit Ability.rb

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,3 +1,17 @@
 class Ability
   include CanCan::Ability
+
+  def initialize(user)
+    user ||= User.new
+
+    can :manage, Post, author_id: user.id
+    can :manage, Comment, author_id: user.id
+    if user.role == 'admin'
+      can :manage, Post
+      can :manage, Comment
+    else
+      can :read, Post
+      can :read, Comment
+    end
+  end
 end
