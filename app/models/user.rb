60ec4a2fceec68c122e7f285722f8947202d7795
camilevahviraki <camilevahviraki@gmail.com>
2022-08-26 14:11:13 +0200

    Add functions for comfirm password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # , :lockable, :timeoutable, :trackable and :omniauthable, :confirmable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :likes
